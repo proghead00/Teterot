2abf02e93e79104029725a1ccaa848475362eb25
unknown <susnata00@gmail.com>
2020-11-20 12:08:47 +0530

    user login red

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,41 @@
+import {
+	USER_LOGIN_FAIL,
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+import axios from "axios";
+
+export const login = (email, password) => async (dispatch) => {
+	try {
+		dispatch({
+			type: USER_LOGIN_REQUEST,
+		});
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const { data } = await axios.post(
+			"/api/users/login",
+			{ email, password },
+			config
+		);
+
+		// after request, dispatch the user login success
+		dispatch({
+			type: USER_LOGIN_SUCCESS,
+			payload: data,
+		});
+
+		localStorage.setItem("userInfo", JSON.stringify(data));
+	} catch (error) {
+		dispatch({
+			type: USER_LOGIN_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
