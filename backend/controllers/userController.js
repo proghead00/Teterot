5030cd9ef2a1bf647def5d15f6d58183c13b302b
unknown <susnata00@gmail.com>
2020-11-20 22:49:47 +0530

    update endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,7 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
 	const userExists = await User.findOne({ email });
 	if (userExists) {
 		res.status(400); // bad req
-		throw new Error("user already exists");
+		throw new Error("User already exists");
 	}
 
 	const user = await User.create({
@@ -76,4 +76,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, registerUser, getUserProfile };
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Pvt
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
